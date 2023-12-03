7924d3e8e12a35ed3626ec8bbc09c9d1de5de1e7
Антон <vip.tank2005@outlook.com>
2023-12-03 23:34:22 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.TitkovAA.Sprint6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.TitkovAA.Sprint6.V18.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
